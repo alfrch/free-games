0523fd0888059a73d798e69b3377ca9caad9e5bc
alfrch <alifrach@gmail.com>
2026-01-16 21:20:35 +0700

    setup project folder structure and add initial home view

---
+++ FreeGames/Module/Home/HomeView.swift
@@ -0,0 +1,34 @@
+//
+//  HomeView.swift
+//  FreeGames
+//
+//  Created by Alif Rachmawan on 16/01/26.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+  
+  var body: some View {
+    ScrollView {
+      VStack(alignment: .center, spacing: 16) {
+        // Header
+        VStack(alignment: .leading, spacing: 4) {
+          Text("Free Games")
+            .font(.largeTitle)
+            .fontWeight(.bold)
+          
+          Text("Discover amazing free games to play")
+            .font(.subheadline)
+            .foregroundStyle(.secondary)
+        }
+        .padding(.horizontal)
+        .padding(.top, 8)
+      }
+    }
+  }
+}
+
+#Preview {
+  HomeView()
+}
