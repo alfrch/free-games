151f20136f0263c3e297020cd05021ddf7ae2209
alfrch <alifrach@gmail.com>
2026-01-16 11:10:37 +0700

    setup project

---
+++ FreeGames/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FreeGames
+//
+//  Created by Alif Rachmawan on 16/01/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    VStack {
+      Image(systemName: "globe")
+        .imageScale(.large)
+        .foregroundStyle(.tint)
+      Text("Hello, world!")
+    }
+    .padding()
+  }
+}
+
+#Preview {
+  ContentView()
+}
---
+++ FreeGames/FreeGamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FreeGamesApp.swift
+//  FreeGames
+//
+//  Created by Alif Rachmawan on 16/01/26.
+//
+
+import SwiftUI
+
+@main
+struct FreeGamesApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
